08ac0edb6128604b54f4a035ae7701efd316c378
GediminasMAt <matijosaitisgediminas@gmail.com>
2022-03-22 09:01:25 +0200

    1 užduotis - done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,14 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+let konvertuoti = document.querySelector(`#submit-btn`).addEventListener(`click`, e => {e.preventDefault();
+  let reiksme = document.querySelector(`#search`).value;
+  document.querySelector(`#output`).innerHTML =`
+  Svoris kilogramais: ${reiksme} <br>
+  Svoris svarais: ${reiksme * 2.2046} <br>
+  Svoris gramais: ${reiksme *0.0010000} <br>
+  Svoris uncijomis: ${reiksme * 35.274}
+  `
+  });
+ 
