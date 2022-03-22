2ba7caa806c4f91e6d875172035f7f11fdec31d0
GediminasMAt <matijosaitisgediminas@gmail.com>
2022-03-22 12:38:34 +0200

    8 Užduotis - Done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,27 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+class Calculator {
+  sum = (sk1, sk2) => {
+    let ats1 = sk1 + sk2;
+    return ats1;
+  };
+  subtraction = (sk1, sk2) => {
+    let ats2 = sk1 - sk2;
+    return ats2;
+  };
+  multiplication = (sk1, sk2) => {
+    let ats2 = sk1 * sk2;
+    return ats2;
+  };
+  division = (sk1, sk2) => {
+    let ats2 = sk1 / sk2;
+    return ats2;
+  };
+}
+
+const checkCalculator = new Calculator();
+console.log("Sudėties ats: " + checkCalculator.sum(2, 2));
+console.log("Atimties ats: " + checkCalculator.subtraction(18, 9));
+console.log("Daugybos ats: " + checkCalculator.multiplication(4, 4));
+console.log("Dalybos ats: " + checkCalculator.division(9, 3));
